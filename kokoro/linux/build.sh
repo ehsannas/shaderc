da5e9f012c6aac6aba6a31d069fe8a62686efde2
Ehsan Nasiri <ehsann@google.com>
2018-01-17 12:05:40 -0500

    Add build instructions for shaderc.

--- kokoro/linux/build.sh
+++ kokoro/linux/build.sh
@@ -16,9 +16,24 @@ unzip -q ninja-linux.zip
 cd $SRC
 git submodule update --init
 
+cd $SRC/third_party
+git clone https://github.com/google/googletest.git
+git clone https://github.com/google/glslang.git
+git clone https://github.com/KhronosGroup/SPIRV-Tools.git spirv-tools
+git clone https://github.com/KhronosGroup/SPIRV-Headers.git spirv-headers
+cd $SRC/
+
+mkdir build
+cd $SRC/build/
+
 # Invoke the build.
 BUILD_SHA=${KOKORO_GITHUB_COMMIT:-$KOKORO_GITHUB_PULL_REQUEST_COMMIT}
 echo $(date): Starting build...
-
+cmake -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo
+ninja
 echo $(date): Build completed.
+
+echo $(date): Starting ctest...
+ctest
+echo $(date): ctest completed.
 
