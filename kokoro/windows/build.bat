b1ed6a702191f9c1a0eeab4c3ac55321927d5a71
Ehsan Nasiri <ehsann@google.com>
2018-01-18 11:12:44 -0500

    windows build script

--- kokoro/windows/build.bat
+++ kokoro/windows/build.bat
@@ -43,6 +43,7 @@ if "%KOKORO_GITHUB_COMMIT%." == "." (
   set BUILD_SHA=%KOKORO_GITHUB_COMMIT%
 )
 cmake -DCMAKE_C_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin/cl.exe" -DCMAKE_CXX_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin/cl.exe" -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+cat "T:/src/github/shaderc/build/CMakeFiles/CMakeTmp"
 if %ERRORLEVEL% GEQ 1 exit /b %ERRORLEVEL%
 ninja
 if %ERRORLEVEL% GEQ 1 exit /b %ERRORLEVEL%
