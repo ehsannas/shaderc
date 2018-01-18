951c3eeca21cb2c499ab1f3c999cc5ef277cf567
Ehsan Nasiri <ehsann@google.com>
2018-01-18 11:41:45 -0500

    windows build script

--- kokoro/windows/build.bat
+++ kokoro/windows/build.bat
@@ -31,7 +31,10 @@ mkdir build
 cd %SRC%\build
 
 :: set up msvc build env
+set Platform="X64"
+set PreferredToolArchitecture="x64"
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+
 
 :: #########################################
 :: Invoke the build.
@@ -42,8 +45,8 @@ if "%KOKORO_GITHUB_COMMIT%." == "." (
 ) else (
   set BUILD_SHA=%KOKORO_GITHUB_COMMIT%
 )
-::cmake -DCMAKE_C_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin/cl.exe" -DCMAKE_CXX_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin/cl.exe" -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
-cmake -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+cmake -DCMAKE_C_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin/cl.exe" -DCMAKE_CXX_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin/cl.exe" -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+::cmake -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
 if %ERRORLEVEL% GEQ 1 exit /b %ERRORLEVEL%
 ninja
 if %ERRORLEVEL% GEQ 1 exit /b %ERRORLEVEL%
